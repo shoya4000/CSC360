8778017b4a3e612bbf30e650a065b7ae500630b2
Sho Ya <shoya@divdot.com>
2019-01-22 18:06:59 -0800

    fgets

--- kapish.c
+++ kapish.c
@@ -11,7 +11,7 @@ January 16th 2019
 int main(int argc, char *argv[]) {
 	char input[512];
 	printf("? ");
-	sscanf("%s", input);
+	fgets(input, 512, stdin);
 	printf("Your input was:\n%s", input);
 	return 0;
 }
