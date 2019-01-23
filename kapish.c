969fbc21ff48da45537bf969b0ec7dd08bf3dead
Sho Ya <shoya@divdot.com>
2019-01-22 18:45:36 -0800

    strip \n

--- kapish.c
+++ kapish.c
@@ -19,6 +19,7 @@ int main(int argc, char *argv[]) {
 		parsed = strtok(input, " ");
 		printf("debug: parsed: <%s>\n", parsed);
 		while (parsed != NULL) {
+			strtok(parsed, "\n");
 			int result = strncmp(parsed, "exit", 512);
 			printf("debug: result: <%d>\n", result);
 			if (strncmp(parsed, "exit", 512) == 0 ) {
