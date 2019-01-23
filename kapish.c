3eb20c2b0867ff7c23d147fe14a2a87cea433f2e
Sho Ya <shoya@divdot.com>
2019-01-22 17:50:51 -0800

    first push

---
+++ kapish.c
@@ -0,0 +1,16 @@
+/*
+Sho Ya Voorthuyzen
+V00730770
+CSC 360
+Assignment #1, Part #1: Doubly linked list - Takes in a number and generates a doubly linked list of each number up to the number given. Can print current list, can insert or delete.
+January 16th 2019
+*/
+#include <stdio.h> //standard input and output library
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+	char *input;
+	printf("? ");
+	scanf("%s", input);
+	return 0;
+}
