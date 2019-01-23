096ad38413b87e8d70820f38edac6e585e4b91ff
Sho Ya <shoya@divdot.com>
2019-01-22 18:40:25 -0800

    add more checks

--- kapish.c
+++ kapish.c
@@ -17,16 +17,16 @@ int main(int argc, char *argv[]) {
 		fgets(input, 512, stdin);
 		char *parsed;
 		parsed = strtok(input, " ");
-		printf("debug: parsed: %s\n", parsed);
+		printf("debug: parsed: <%s>\n", parsed);
 		while (parsed != NULL) {
 			int result = strcmp(parsed, "exit");
-			printf("debug: result: %d\n", result);
-			if (strcmp(parsed, "exit") == 0 ) {
+			printf("debug: result: <%d>\n", result);
+			if (strncmp(parsed, "exit") == 0 ) {
 				printf("ding");
 				exit = true;
 				break;
 			}
-			printf("debug: parsed: %s\n", parsed);
+			printf("debug: parsed: <%s>\n", parsed);
 			parsed = strtok(NULL, " ");
 		}
 	}
