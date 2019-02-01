e0268b997106c4d57740a6b7feee52a6f5351e6f
Sho Ya <shoya@divdot.com>
2019-01-31 18:28:00 -0800

    fix printf

--- kapish.c
+++ kapish.c
@@ -55,7 +55,7 @@ int main(int argc, char *argv[]) {
 				}
 				pid_t childID = fork();
 				if (childID == 0) {
-					print("%s", execvp(argv[0], argv));
+					printf("%s", execvp(argv[0], argv));
 				} else {
 					wait(NULL);
 				}
