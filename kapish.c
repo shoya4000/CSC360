3a6e7e4222126c90315c69fa63065d7242b3d561
Sho Ya <shoya@divdot.com>
2019-01-31 20:36:30 -0800

    add debug

--- kapish.c
+++ kapish.c
@@ -63,6 +63,7 @@ char* reexecute(char* prefix) {
 	for (i = 0; i < strlen(prefix) - 1; i++) {
 		prefix[i] = prefix[i + 1];
 	}
+	printf("prefix: %s", prefix);
 	struct node *item = START;
 	for (i = 0; i < historyLength; i++) {
 		if (strncmp(prefix, item->the_command, strlen(prefix)) == 0) {
