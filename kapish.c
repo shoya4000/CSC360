ac63130344aabc7ddf325666bb6229bf22b5b86c
Sho Ya <shoya@divdot.com>
2019-01-31 18:15:23 -0800

    implement execvp and wait

--- kapish.c
+++ kapish.c
@@ -1,10 +1,3 @@
-/*
-Sho Ya Voorthuyzen
-V00730770
-CSC 360
-Assignment #1, Part #1: Doubly linked list - Takes in a number and generates a doubly linked list of each number up to the number given. Can print current list, can insert or delete.
-January 16th 2019
-*/
 #include <stdio.h> //standard input and output library
 #include <stdlib.h>
 #include <string.h>
@@ -51,6 +44,15 @@ int main(int argc, char *argv[]) {
 			}
 			else if (strncmp(parsed, "exit", 512) == 0 ) {
 				exit(EXIT_SUCCESS);
+			}
+			else {
+				pid_t childID = fork()
+				if (childID == 0) {
+					execvp(parsed);
+				} else {
+					wait(NULL);
+				}
+
 			}
 			parsed = strtok(NULL, " ");
 		}
