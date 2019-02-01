0608e5da52474c87b025d6f98990c79e1758cdae
Sho Ya <shoya@divdot.com>
2019-01-31 20:52:02 -0800

    try to fix mem leak4

--- kapish.c
+++ kapish.c
@@ -25,7 +25,7 @@ struct node *END = NULL;
 void addHistory(char* command) {
 	struct node *newNode = malloc(sizeof * newNode);
 	check_mem_fail(newNode);
-	newNode->the_command = malloc(sizeof * command);
+	newNode->the_command = malloc(sizeof * strlen(command + 1));
 	check_mem_fail(newNode->the_command);
 	strcpy(newNode->the_command, command);
 	newNode->prev = NULL;
