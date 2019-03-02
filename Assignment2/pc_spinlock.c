52c94fe942a5e46aea5805d71fb3d39b1f298d1c
Sho Ya <shoya@divdot.com>
2019-03-01 17:04:53 -0800

    try jon

--- Assignment2/pc_spinlock.c
+++ Assignment2/pc_spinlock.c
@@ -17,50 +17,55 @@ int histogram [MAX_ITEMS + 1]; // histogram [i] == # of times list stored i item
 int items = 0;
 
 void* producer (void* v) {
-  spinlock_t *lock = (spinlock_t *) v;
+  volatile spinlock_t *lock = (spinlock_t *) v;
+
   for (int i = 0; i < NUM_ITERATIONS; i++) {
-    // TODO
     spinlock_lock(lock);
     if (items < MAX_ITEMS) {
-      items++;
+      items += 1;
     } else {
-      producer_wait_count++;
+      producer_wait_count += 1;
     }
-    histogram[items]++;
+    histogram[items] += 1;
     spinlock_unlock(lock);
   }
   return NULL;
 }
 
 void* consumer (void* v) {
+  volatile spinlock_t *lock = (spinlock_t *) v;
+
   for (int i = 0; i < NUM_ITERATIONS; i++) {
-    // TODO
-    spinlock_t *lock = (spinlock_t *) v;
     spinlock_lock(lock);
     if (items > 0) {
-      items--;
+      items -= 1;
     } else {
-      consumer_wait_count++;
+      consumer_wait_count += 1;
     }
-    histogram[items]++;
+    histogram[items] += 1;
     spinlock_unlock(lock);
   }
   return NULL;
 }
 
 int main (int argc, char** argv) {
-  uthread_t t[4];
+  int THREAD_COUNT = NUM_CONSUMERS + NUM_PRODUCERS;
 
-  uthread_init (4);
+  uthread_t t[THREAD_COUNT];
+  uthread_init (THREAD_COUNT);
 
-  // TODO: Create Threads and Join
   spinlock_t lock;
   spinlock_create(&lock);
-  t[0] = uthread_create(producer, (void *)&lock);
-  t[1] = uthread_create(consumer, (void *)&lock);
-  t[2] = uthread_create(producer, (void *)&lock);
-  t[3] = uthread_create(consumer, (void *)&lock);
-  for (int i = 0; i < 4; i++) {
+
+  for (int i = 0; i < THREAD_COUNT; i++) {
+    if (i % 2 == 0) {
+      t[i] = uthread_create(&consumer, (void *)&lock);
+    } else {
+      t[i] = uthread_create(&producer, (void *)&lock);
+    }
+  }
+
+  for (int i = 0; i < THREAD_COUNT; i++) {
     uthread_join(t[i], NULL);
   }
 
