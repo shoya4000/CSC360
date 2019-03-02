edfbef5bc6e5e5c49e1a49e36bde958d610a69af
Sho Ya <shoya@divdot.com>
2019-03-01 22:30:25 -0800

    do pc_sem

--- Assignment2/pc_sem.c
+++ Assignment2/pc_sem.c
@@ -13,9 +13,22 @@ int histogram [MAX_ITEMS + 1]; // histogram [i] == # of times list stored i item
 
 int items = 0;
 
+struct thread_start_arg {
+  uthread_sem_t lock;
+  uthread_sem_t produce;
+  uthread_sem_t consume;
+};
+
 void* producer (void* v) {
   for (int i = 0; i < NUM_ITERATIONS; i++) {
     // TODO
+    struct thread_start_arg *start_arg = (struct thread_start_arg*)v;
+    uthread_sem_wait(start_arg->produce);
+    uthread_sem_wait(start_arg->lock);
+    items++;
+    histogram[items]++;
+    uthread_sem_signal(start_arg->lock);
+    uthread_sem_signal(start_arg->consumer);
   }
   return NULL;
 }
@@ -23,6 +36,13 @@ void* producer (void* v) {
 void* consumer (void* v) {
   for (int i = 0; i < NUM_ITERATIONS; i++) {
     // TODO
+    struct thread_start_arg *start_arg = (struct thread_start_arg*)v;
+    uthread_sem_wait(start_arg->consume);
+    uthread_sem_wait(start_arg->lock);
+    items--;
+    histogram[items]++;
+    uthread_sem_signal(start_arg->lock);
+    uthread_sem_signal(start_arg->produce);
   }
   return NULL;
 }
@@ -33,6 +53,21 @@ int main (int argc, char** argv) {
   uthread_init (4);
 
   // TODO: Create Threads and Join
+  struct thread_start_arg *start_arg = malloc(sizeof(struct thread_start_arg));
+  start_arg->lock = uthread_sem_create(1);
+  start_arg->produce = uthread_cond_create(MAX_ITEMS);
+  start_arg->consume = uthread_sem_create(0);
+  t[0] = uthread_create(&producer, start_arg);
+  t[1] = uthread_create(&consumer, start_arg);
+  t[2] = uthread_create(&producer, start_arg);
+  t[3] = uthread_create(&consumer, start_arg);
+  for (int i = 0; i < 4; i++) {
+    uthread_join(t[i], NULL);
+  }
+  uthread_sem_destroy(start_arg->produce);
+  uthread_sem_destroy(start_arg->consume);
+  uthread_sem_destroy(start_arg->lock);
+  free(start_arg);
 
   printf ("items value histogram:\n");
   int sum = 0;
