b52272b8faaf3254ab3ddf2525f67149eb92605d
Sho Ya <shoya@divdot.com>
2019-03-01 18:17:36 -0800

    try pc_mutex_cond4

--- Assignment2/pc_mutex_cond.c
+++ Assignment2/pc_mutex_cond.c
@@ -55,10 +55,10 @@ int main (int argc, char** argv) {
 
   // TODO: Create Threads and Join
   uthread_mutex_t mt;
-  t[0] = uthread_create(producer, (void *)mt);
-  t[1] = uthread_create(consumer, (void *)mt);
-  t[2] = uthread_create(producer, (void *)mt);
-  t[3] = uthread_create(consumer, (void *)mt);
+  t[0] = uthread_create(producer, mt);
+  t[1] = uthread_create(consumer, mt);
+  t[2] = uthread_create(producer, mt);
+  t[3] = uthread_create(consumer, mt);
   for (int i = 0; i < 4; i++) {
     uthread_join(t[i], NULL);
   }
