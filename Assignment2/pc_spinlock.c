86ab61d8ba77989462125cdbc8a931ddbe50a36d
Sho Ya <shoya@divdot.com>
2019-03-01 16:59:29 -0800

    try break

--- Assignment2/pc_spinlock.c
+++ Assignment2/pc_spinlock.c
@@ -17,9 +17,9 @@ int histogram [MAX_ITEMS + 1]; // histogram [i] == # of times list stored i item
 int items = 0;
 
 void* producer (void* v) {
+  spinlock_t *lock = (spinlock_t *) v;
   for (int i = 0; i < NUM_ITERATIONS; i++) {
     // TODO
-    spinlock_t *lock = (spinlock_t *) v;
     spinlock_lock(lock);
     if (items < MAX_ITEMS) {
       items++;
