405b3f7d74ae61f874d1886613aa752d86ccd0c1
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-11 02:09:56 -0700

    init superblock

--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -10,15 +10,15 @@ const int MAGIC_NUMBER = 0x6b6f6d61;
 const int INODE_SIZE = 32;
 const int INODE_COUNT = 2048;
 
-struct Superblock {
-	uint32_t magNum, blockNum, inodeNum, inodeHead;
+struct Super {
+	int magNum, blockNum, inodeNum, inodeHead;
 };
 
 void initLLFS(FILE* disk) {
-	struct Superblock super = {
+	struct Super superInit = {
 		MAGIC_NUMBER, NUM_BLOCKS, INODE_COUNT, 0
 	};
-	writeBlock(disk, 0, &super, sizeof(super));
+	writeBlock(disk, 0, &superInit, sizeof(super));
 }
 
 char* createEmptyInode() {
