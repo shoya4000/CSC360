34e50a7e584e3311833463d704b30cf9b99e929f
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-10 20:08:46 -0700

    test new makefile

--- Assignment3/apps/test01.c
+++ Assignment3/apps/test01.c
@@ -1,6 +1,35 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+
+void removeNewLine(char* string) {
+	if (string[strlen(string) - 1] == '\n') {
+		string[strlen(string) - 1] = '\0';
+	}
+}
 
 int main(int argc, char* argv[]) {
-	printf("Hello World\n");
+	while (true) {
+		printf("0_o ");
+		char input[512];
+		if (fgets(input, 512, stdin) == NULL) {
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		}
+		removeNewLine(input);
+		char *parsed;
+		parsed = strtok(input, " ");
+		while (parsed != NULL) {
+			if (strncmp(parsed, "InitLLFS", 512) == 0 ) {
+				createDisk();
+				break;
+			}
+			else if (strncmp(parsed, "exit", 512) == 0 ) {
+				exit(EXIT_SUCCESS);
+			}
+			parsed = strtok(NULL, " ");
+		}
+	}
 	return 0;
 }
--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -13,34 +13,9 @@ void removeNewLine(char* string) {
 }
 
 void createDisk() {
-	FILE* disk = fopen("../disk/vdisk", "wb"); // Open the file to be written to in binary mode
+	FILE* disk = fopen("vdisk", "wb"); // Open the file to be written to in binary mode
 	char* init = calloc(BLOCK_SIZE * NUM_BLOCKS, 1);
 	fwrite(init, BLOCK_SIZE * NUM_BLOCKS, 1, disk);
 
 	fclose(disk);
-}
-
-int main(int argc, char* argv[]) {
-	while (true) {
-		printf("0_o ");
-		char input[512];
-		if (fgets(input, 512, stdin) == NULL) {
-			printf("\n");
-			exit(EXIT_SUCCESS);
-		}
-		removeNewLine(input);
-		char *parsed;
-		parsed = strtok(input, " ");
-		while (parsed != NULL) {
-			if (strncmp(parsed, "InitLLFS", 512) == 0 ) {
-				createDisk();
-				break;
-			}
-			else if (strncmp(parsed, "exit", 512) == 0 ) {
-				exit(EXIT_SUCCESS);
-			}
-			parsed = strtok(NULL, " ");
-		}
-	}
-	return 0;
 }
