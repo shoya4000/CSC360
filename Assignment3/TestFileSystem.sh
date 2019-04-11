eea7dff4d17aedae8112faee97b6da8041023fe2
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-10 20:47:12 -0700

    more makefile work

--- Assignment3/TestFileSystem.sh
+++ Assignment3/TestFileSystem.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-rm -r disk; mkdir disk
 cd io
 make
 ./File
--- Assignment3/disk/Disk.c
+++ Assignment3/disk/Disk.c
@@ -1,12 +1,12 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#include <File.h>
+#include <Disk.h>
 
 const int BLOCK_SIZE = 512;
 const int NUM_BLOCKS = 4096;
 
-void newDisk() {
+void createDisk() {
 	FILE* disk = fopen("vdisk", "wb"); // Open the file to be written to in binary mode
 	char* init = calloc(BLOCK_SIZE * NUM_BLOCKS, 1);
 	fwrite(init, BLOCK_SIZE * NUM_BLOCKS, 1, disk);
