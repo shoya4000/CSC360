23fac39c609c21b9c7a8e19b64dc155979cd2d7a
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-10 19:57:47 -0700

    make File.c interactable

--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -3,6 +3,12 @@
 const int BLOCK_SIZE = 512;
 const int NUM_BLOCKS = 4096;
 
+void removeNewLine(char* string) {
+	if (string[strlen(string) - 1] == '\n') {
+		string[strlen(string) - 1] = '\0';
+	}
+}
+
 void createDisk() {
 	FILE* disk = fopen("../disk/vdisk", "wb"); // Open the file to be written to in binary mode
 	char* init = calloc(BLOCK_SIZE * NUM_BLOCKS, 1);
@@ -12,6 +18,24 @@ void createDisk() {
 }
 
 int main(int argc, char* argv[]) {
-	createDisk();
+	while (true) {
+		printf("0_o ");
+		char input[512];
+		if (fgets(input, 512, stdin) == NULL) {
+			printf("\n");
+			exit(EXIT_SUCCESS);
+		}
+		removeNewLine(input);
+		while (parsed != NULL) {
+			if (strncmp(parsed, "InitLLFS", 512) == 0 ) {
+				createDisk();
+				break;
+			}
+			else if (strncmp(parsed, "exit", 512) == 0 ) {
+				exit(EXIT_SUCCESS);
+			}
+			parsed = strtok(NULL, " ");
+		}
+	}
 	return 0;
 }
