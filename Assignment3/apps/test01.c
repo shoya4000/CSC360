d6bc65c81660c449e4e704339dc5ceff5128654d
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-10 20:11:14 -0700

    test test01

--- Assignment3/apps/test01.c
+++ Assignment3/apps/test01.c
@@ -3,7 +3,15 @@
 #include <string.h>
 #include <stdbool.h>
 
-#include <File.h>
+//#include <File.h>
+
+void createDisk() {
+	FILE* disk = fopen("vdisk", "wb"); // Open the file to be written to in binary mode
+	char* init = calloc(BLOCK_SIZE * NUM_BLOCKS, 1);
+	fwrite(init, BLOCK_SIZE * NUM_BLOCKS, 1, disk);
+
+	fclose(disk);
+}
 
 void removeNewLine(char* string) {
 	if (string[strlen(string) - 1] == '\n') {
--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -1,16 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
-#include <stdbool.h>
 
 const int BLOCK_SIZE = 512;
 const int NUM_BLOCKS = 4096;
-
-void removeNewLine(char* string) {
-	if (string[strlen(string) - 1] == '\n') {
-		string[strlen(string) - 1] = '\0';
-	}
-}
 
 void createDisk() {
 	FILE* disk = fopen("vdisk", "wb"); // Open the file to be written to in binary mode
