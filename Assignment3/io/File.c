7cb54ef64e46af8ce054cf8a3300e2e85a3c2c77
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-11 01:58:42 -0700

    init num_blocks32

--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -6,8 +6,9 @@
 #include <File.h>
 #include <Disk.h>
 
-const int INODE_SIZE = 32;
 const int MAGIC_NUMBER = 0x6b6f6d61;
+const int INODE_SIZE = 32;
+const int INODE_COUNT = 2048;
 
 struct Superblock {
 	uint32_t magic;
@@ -17,7 +18,7 @@ void initLLFS(FILE* disk) {
 	struct Superblock super = {
 		.magic = MAGIC_NUMBER
 	};
-	writeBlock(disk, 0, &super, sizeof(super));
+	writeBlock(disk, 0, &MAGIC_NUMBER, sizeof(super));
 }
 
 char* createEmptyInode() {
