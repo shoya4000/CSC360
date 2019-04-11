8010b50b6e965a072c2fe30642a7f4508842c7f7
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-10 17:31:32 -0700

    test createDisk

--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -1,6 +1,17 @@
 #include <stdio.h>
+#include <stdlib.h>
+const int BLOCK_SIZE = 512;
+const int NUM_BLOCKS = 4096;
+
+void createDisk() {
+	FILE* disk = fopen("../disk/vdisk", "wb"); // Open the file to be written to in binary mode
+	char* init = calloc(BLOCK_SIZE * NUM_BLOCKS, 1);
+	fwrite(init, BLOCK_SIZE * NUM_BLOCKS, 1, disk);
+
+	fclose(disk);
+}
 
 int main(int argc, char* argv[]) {
-	printf("Hello World\n");
+	createDisk();
 	return 0;
 }
