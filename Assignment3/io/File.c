665c211f1a94f9d55e857820eefe3e86a2262ed9
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-11 14:44:41 -0700

    write freeblock

--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -13,12 +13,18 @@ const int INODE_COUNT = 2048;
 struct Super {
 	int mag, blocks, inodes, head;
 };
+struct FreeBlockVector {
+	unsigned int superBlock, freeBlocks, reservedBlocks, rest;
+}
 
 void initLLFS(FILE* disk) {
 	struct Super superInit = {
 		MAGIC_NUMBER, NUM_BLOCKS, INODE_COUNT, 0
 	};
 	writeBlock(disk, 0, &superInit, sizeof(superInit));
+	struct FreeBlockVector {
+		0 : 1, 0 : 1, 0 : 8, 0xf : 8
+	};
 }
 
 char* createEmptyInode() {
