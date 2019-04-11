4661dd7af7c6c7328b9e332cf8c560a47e107e13
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-11 16:46:38 -0700

    write freeblock47

--- Assignment3/apps/test01.c
+++ Assignment3/apps/test01.c
@@ -12,26 +12,26 @@ int main(int argc, char* argv[]) {
 
 	printf("Creating disk...\n");
 	createDisk();
-	printf("Empty disk created...\n");
+	printf("Empty disk created\n");
 
-	printf("Accessing vdisk\n");
+	printf("Accessing vdisk..\n");
 	FILE* disk = fopen("vdisk", "rb+");
 
-	printf("Initializing LLFS\n");
+	printf("Initializing LLFS..\n");
 	initLLFS(disk);
 	printf("LLFS initialized\n");
 
-	printf("Reading SuperBlock\n");
+	printf("Reading SuperBlock...\n");
 	char* buffer1 = (char*)malloc(BLOCK_SIZE);
 	readBlock(disk, 0, buffer1);
 	printf("%s\n", buffer1);
 	free(buffer1);
 
-	printf("Reading Free Block Vector\n");
+	printf("Reading Free Block Vector...\n");
 	int* buffer2 = (int*)malloc(NUM_BLOCKS);
 	readBlock(disk, 1, buffer2);
 
-	printf("Confirming blocks 0-9 reserved\n");
+	printf("Confirming blocks 0-9 reserved...\n");
 	int i;
 	for (i = 0; i < 10; i++) {
 		if (TestBit(buffer2, i) != 0) {
@@ -41,7 +41,7 @@ int main(int argc, char* argv[]) {
 	}
 
 	printf("Blocks 0-9 are reserved\n");
-	printf("Confirming blocks 10-4095 free\n");
+	printf("Confirming blocks 10-4095 free...\n");
 	for (i = 10; i < NUM_BLOCKS; i++) {
 		if (!TestBit(buffer2, i) != 0) {
 			printf("Error in block reservation\n");
