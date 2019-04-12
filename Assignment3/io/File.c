ba41178172b388182e67533a541a81330afb353d
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-11 22:44:12 -0700

    test createfile34

--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -80,13 +80,13 @@ void createFile(FILE* disk) {
 	struct Inode inode = {
 		.direct[0] = 132 //132 and other values will need to be free blocks that are found
 	};
-	int offset = findFirstFreeInode(disk);
+	//int offset = findFirstFreeInode(disk);
 
 	char* inodesBuffer = (char*)malloc(BLOCK_SIZE);
 	readBlock(disk, 3, inodesBuffer);
-	memcpy(inodesBuffer + (offset * INODE_SIZE), &inode, INODE_SIZE);
+	memcpy(inodesBuffer, &inode, INODE_SIZE);
 
-	writeBlock(disk, 3, inodesBuffer, INODE_SIZE * (offset + 1));
+	writeBlock(disk, 3, inodesBuffer, INODE_SIZE);
 }
 
 void writeToFile(FILE* disk, void* data, int size) {
