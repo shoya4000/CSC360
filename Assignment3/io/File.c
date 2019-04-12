dd37cddb888bd7f7d232080c11729762aaf09564
Sho Ya <mikevoorthuyzen@hotmail.com>
2019-04-11 22:41:03 -0700

    test createfile31

--- Assignment3/io/File.c
+++ Assignment3/io/File.c
@@ -84,9 +84,9 @@ void createFile(FILE* disk) {
 
 	char* inodesBuffer = (char*)malloc(BLOCK_SIZE);
 	readBlock(disk, 3, inodesBuffer);
-	memcpy(inodesBuffer, &inode, INODE_SIZE);
+	memcpy(inodesBuffer + (offset * INODE_SIZE), &inode, INODE_SIZE);
 
-	writeBlock(disk, 3, inodesBuffer, INODE_SIZE);
+	writeBlock(disk, 3, inodesBuffer, INODE_SIZE * (offset + 1);
 }
 
 void writeToFile(FILE* disk, void* data, int size) {
